49a81a49809c172cbc0b4c43aa47a99738a1ab4a
ZackAtambo <zacharyatambo@gmail.com>
2023-12-14 00:01:51 -0800

    brainfuck

---
+++ monty.h
@@ -0,0 +1,75 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <ctype.h>
+
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
+} stack_t;
+extern stack_t *head;
+
+/**
+ * struct instruction_s - opcoode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+#define getline
+#define free_stack
+void push(stack_t **stack, unsigned int line);
+void pall(stack_t **stack, unsigned int line);
+void pint(stack_t **stack, unsigned int line);
+void pop(stack_t **stack, unsigned int line);
+void swap(stack_t **stack, unsigned int line);
+void nop(stack_t **stack, unsigned int line);
+int main(int argc, char *argv[]);
+instruction_t *parse(char *line);
+char *extract_opcode(char *string);
+void add(stack_t **stack, unsigned int line);
+void sub(stack_t **stack, unsigned int line);
+void division(stack_t **stack, unsigned int line);
+void mul(stack_t **stack, unsigned int line);
+void mod(stack_t **stack, unsigned int line);
+void pchar(stack_t **stack, unsigned int line);
+void pstr(stack_t **stack, unsigned int line);
+void rotl(stack_t **stack, unsigned int line);
+void rotr(stack_t **stack, unsigned int line);
+ void freenodes(void);
+ void queue(stack_t **stack, unsigned int line);
+int line_parse(char *buf, int line);
+void opcode_mapping(char *opcode, char *opvalue, unsigned int line, int rt_val);
+typedef void (*op_func)(stack_t **, unsigned int);
+void execute(op_func func, char *opc, char *opv, unsigned int line, int rt_val);
+stack_t *newnode(int n);
+void mod(stack_t **stack, unsigned int line);
+void show_top(stack_t **stack, unsigned int line);
+
+
+#endif
---
+++ parser.c
@@ -0,0 +1,147 @@
+#include "monty.h"
+
+/**
+*line_parse - parse through individual lines from stream
+*@buf: line from file
+*@line: line number
+*Return: Void
+*/
+
+int line_parse(char *buf, int line)
+{
+	char *opcode = NULL, *opvalue = NULL, *delim = " \n";
+	int rt_val = 0;
+
+	if (buf == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed");
+		exit(EXIT_FAILURE);
+	}
+
+	opcode = strtok(buf, delim);
+	if (opcode == NULL)
+		return (rt_val);
+	opvalue = strtok(NULL, delim);
+
+	if (strcmp(opcode, "stack") == 0)
+		return (0);
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+
+	opcode_mapping(opcode, opvalue, line, rt_val);
+	return (rt_val);
+}
+
+/**
+*opcode_mapping - links an 'opcode' to corresponding function
+*@opcode: operation code
+*@opvalue: operation argument
+*@line: line umber
+*@rt_val: ...
+*Return: Void
+*/
+
+void opcode_mapping(char *opcode, char *opvalue, unsigned int line, int rt_val)
+{
+	int i = 0, flag;
+
+	instruction_t opsmap[] = {
+	{"push", push},
+	{"pall", pall},
+	{"pint", show_top},
+	{"add", add},
+	{"swap", swap},
+	{"pop", pop},
+	{"nop", nop},
+	{"sub", sub},
+	{"div", division},
+	{"mul", mul},
+	{"mod", mod},
+	{"pchar", pchar},
+	{"pstr", pstr},
+	{"rotl", rotl},
+	{"rotr", rotr},
+	{NULL, NULL}
+	};
+
+	if (opcode[0] == '#')
+		return;
+
+	for (flag = 1, i = 0; opsmap[i].opcode != NULL; i++)
+	{
+		if (strcmp(opcode, opsmap[i].opcode) == 0)
+		{
+			execute(opsmap[i].f, opcode, opvalue, line, rt_val);
+			flag = 0;
+			/*return;*/
+		}
+	}
+	if (flag == 1)
+	{
+		fprintf(stderr, "L%u: unknown instruction %s\n", line, opcode);
+		exit(EXIT_FAILURE);
+	}
+}
+
+/**
+*execute - executes an operation based on an opcode
+*@index: function to be executed
+*@opc: operation code
+*@opv: operation argument
+*@line: line number in file
+*@rt_val: ....
+*
+*/
+
+void execute(op_func index, char *opc, char *opv, unsigned int line, int rt_val)
+{
+	stack_t *node;
+	int i, flag;
+
+	flag = 1;
+	if (strcmp(opc, "push") == 0)
+	{
+		if (opv != NULL && opv[0] == '-')
+		{
+			opv = opv + 1;
+			flag = -1;
+		}
+		if (opv == NULL)
+		{
+			fprintf(stderr, "L%u: usage: push integer\n", line);
+			exit(EXIT_FAILURE);
+		}
+		for (i = 0; opv[i] != '\0'; i++)
+		{
+			if (isdigit(opv[i]) == 0)
+			{
+				fprintf(stderr, "L%d: usage: push integer\n", line);
+				exit(EXIT_FAILURE);
+			}
+		}
+		node = newnode(atoi(opv) * flag);
+		if (rt_val == 0)
+			index(&node, line);
+	}
+	else
+                index(&head, line);
+}
+
+/**
+ *newnode - creates a new doubly linked list node
+ *@n: data to be inserted into node
+ *Return: pointer to new node
+ */
+
+stack_t *newnode(int n)
+{
+	stack_t *node;
+
+	node = malloc(sizeof(stack_t));
+	if (node == NULL)
+		fprintf(stderr, "Error: Malloc failed\n");
+	node->next = NULL;
+	node->prev = NULL;
+	node->n = n;
+	return (node);
+}
